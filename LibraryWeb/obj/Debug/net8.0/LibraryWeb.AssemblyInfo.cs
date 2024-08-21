86926aa260694c60134a852aa204844c8ba462e6
emrehansmsk194 <emrehansimsek194@gmail.com>
2024-08-21 15:48:17 +0300

    Pull trying.

--- LibraryWeb/obj/Debug/net8.0/LibraryWeb.AssemblyInfo.cs
+++ LibraryWeb/obj/Debug/net8.0/LibraryWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ddf9c70c8f0f0bfdc922d8d5f9d26b5ae25dc97")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
